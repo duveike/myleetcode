59ba4a598d8edc1211e82b3638723a95028ab7f9
duveike <yp_chang@qq.com>
2022-04-06 22:21:43 +0800

    commmit lc134

--- src/com/duweike/lc134/LeetCode134.java
+++ src/com/duweike/lc134/LeetCode134.java
@@ -3,7 +3,23 @@ package com.duweike.lc134;
 public class LeetCode134 {
 
     public static int canCompleteCircuit(int[] gas, int[] cost) {
-        int[] gas2 = new int[2*gas.length];
+        int remain=0;
+        int debet=0;
+        int idx=0;
+        int len=gas.length;
+        for(int i=0;i<len;i++)
+        {
+            remain+=gas[i]-cost[i];
+            if(remain<0)
+            {
+                debet+=remain;
+                idx=i+1;
+                remain=0;
+            }
+        }
+        return remain+debet>=0?idx:-1;
+
+       /* int[] gas2 = new int[2*gas.length];
         int[] cost2 = new int[2*cost.length];
         for(int i=0;i<gas.length;i++) {
             gas2[i] = gas[i];
@@ -15,8 +31,9 @@ public class LeetCode134 {
         }
 
         int sum = 0;
-        for(int i=0;i<gas.length;i++) {
+        for(int i=0;i<gas.length;) {
             if(cost[i]>sum+gas[i]) {
+                i++;
                 continue;
             } else {
                 int j=i;
@@ -28,13 +45,19 @@ public class LeetCode134 {
 
                 }
                 if(j >= i+gas.length) return i;
+                else i=j;
             }
             sum = 0;
         }
-        return -1;
+        return -1;*/
     }
 
     public static void main(String[] args) {
+
+        int[]gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        System.out.println(canCompleteCircuit(gas,cost));
+
 
     }
 }
